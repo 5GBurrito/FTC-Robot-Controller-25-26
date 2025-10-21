aaf0b5956b453b2c22289d7d5704bb64d9e5f591
5gburrito <huskyscout1405@gmail.com>
2025-10-21 16:33:10 -0500

    NVM i fixed it now

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveTrainExample.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveTrainExample.java
@@ -7,25 +7,26 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 
 @TeleOp
 public class DriveTrainExample extends OpMode {
+
+    //mecanum motors
+    private DcMotor frontLeftMotor;
+    private DcMotor frontRightMotor;
+    private DcMotor backLeftMotor;
+    private DcMotor backRightMotor;
+
     @Override
     public void init() {
-        //mecanum motors
-        private DcMotor frontLeftMotor;
-        private DcMotor frontRightMotor;
-        private DcMotor backLeftMotor;
-        private DcMotor backRightMotor;
-    }
-
-    @Override
-    public void loop() {
-
         //Motor Initilization
         frontLeftMotor = hardwareMap.dcMotor.get("frontLeft");
         frontRightMotor = hardwareMap.dcMotor.get("frontRight");
         backLeftMotor = hardwareMap.dcMotor.get("backLeft");
         backRightMotor = hardwareMap.dcMotor.get("backRight");
+    }
 
+    @Override
+    public void loop() {
         //mecanum code
+        //left stick is forward, backward and strafe left and right respectively, right stick left and right is turning.
         double y = -gamepad1.left_stick_y; // Remember, Y stick value is reversed
         double x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
         double rx = gamepad1.right_stick_x;
@@ -39,6 +40,9 @@ public class DriveTrainExample extends OpMode {
         double frontRightPower = (y - x - rx) / denominator;
         double backRightPower = (y + x - rx) / denominator;
 
+
+        //Mecanum to Motor, Hunter you will need to change which values are negative,
+        // refer to this link for which wat the motors should be moving https://gm0.org/en/latest/_images/mecanum-drive-directions.png
         frontLeftMotor.setPower(-frontLeftPower);
         backLeftMotor.setPower(-backLeftPower);
         frontRightMotor.setPower(frontRightPower);
